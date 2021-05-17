695ae9166bf99bef79f0f7264b935bbe70a751cd
丁志超 <948351520@qq.com>
2021-05-17 10:11:18 +0800

    no message

--- swan-common/src/main/java/org/zoo/swan/common/jedis/JedisClientSingle.java
+++ swan-common/src/main/java/org/zoo/swan/common/jedis/JedisClientSingle.java
@@ -17,14 +17,12 @@
 
 package org.zoo.swan.common.jedis;
 
-import java.util.Objects;
-
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zoo.swan.common.config.SwanConfig;
-import org.zoo.swan.common.constant.CommonConstant;
+import org.zoo.swan.common.utils.DateUtils;
 import org.zoo.swan.common.utils.RepositoryPathUtils;
 
 /**
@@ -32,70 +30,63 @@ import org.zoo.swan.common.utils.RepositoryPathUtils;
  * @author dzc
  */
 public class JedisClientSingle implements JedisClient {
-	private static final Logger LOGGER = LoggerFactory.getLogger(JedisClientSingle.class);
+	/**
+     * logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(JedisClientCluster.class);
 
     private RedissonClient redissonClient = null;
     
     private SwanConfig swanConfig = null;
     
-	/**历史布隆过滤器*/
-	RBloomFilter<String> historyBloomFilter = null;
-	
 	/**当前布隆过滤器*/
 	RBloomFilter<String> nowBloomFilter = null;
+	
+	/**昨天布隆过滤器*/
+	RBloomFilter<String> historyBloomFilter = null;
 	
 	
 	public JedisClientSingle(RedissonClient redissonClient, SwanConfig swanConfig) {
 		this.redissonClient = redissonClient;
 		this.swanConfig = swanConfig;
-		initBloomFilter();	
 	}
 
 	/**初始化布隆过滤器
 	 * @param swanConfig */
-	public synchronized RBloomFilter<String> initBloomFilter() {
+	public void initBloomFilter() {
 		//当前布隆过滤器初始化
-		RBloomFilter<String> nowBloomFilter = redissonClient.getBloomFilter(RepositoryPathUtils.buildBloomFilterKey(CommonConstant.Now,swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
+		RBloomFilter<String> nowBloomFilter = redissonClient.getBloomFilter(RepositoryPathUtils.buildBloomFilterKey(DateUtils.subDay(0),swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
 		nowBloomFilter.tryInit(swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getTotalNum(),swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getErrorRate());
 		
-		//历史布隆过滤器初始化
-		RBloomFilter<String> historyBloomFilter = redissonClient.getBloomFilter(RepositoryPathUtils.buildBloomFilterKey(CommonConstant.History,swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
-		nowBloomFilter.tryInit(swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getTotalNum(),swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getErrorRate());
+		//昨天布隆过滤器初始化
+		RBloomFilter<String> historyBloomFilter = redissonClient.getBloomFilter(RepositoryPathUtils.buildBloomFilterKey(DateUtils.subDay(-1),swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
+		historyBloomFilter.tryInit(swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getTotalNum(),swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getErrorRate());
 		
-		LOGGER.info("布隆过滤器初始化成功,容错率:{},预计已经插入数量:{},容量:{},内存使用量:{}bytes",nowBloomFilter.getFalseProbability(),nowBloomFilter.count(),nowBloomFilter.getSize(),nowBloomFilter.sizeInMemoryAsync()); 
+		
+		LOGGER.info("布隆过滤器初始化成功,容错率:{},预计已经插入数量:{},容量:{}",nowBloomFilter.getFalseProbability(),nowBloomFilter.count(),nowBloomFilter.getSize()); 
 		this.nowBloomFilter = nowBloomFilter;
-		this.historyBloomFilter = historyBloomFilter;
-		return nowBloomFilter;
+        this.historyBloomFilter = historyBloomFilter;
 	}
 
 	@Override
 	public boolean addToRBloomFilter(String key) {
-		if(Objects.isNull(nowBloomFilter)) {
-			initBloomFilter();
-		}
+		initBloomFilter();
 		return nowBloomFilter.add(key);
 	}
 
 	@Override
 	public boolean isContainsInRBloomFilter(String key) {
-		if(Objects.isNull(nowBloomFilter) && Objects.isNull(historyBloomFilter)) {
-			initBloomFilter();
-		}
-		
+		initBloomFilter();
 		return nowBloomFilter.contains(key)==true?nowBloomFilter.contains(key):historyBloomFilter.contains(key);
 	}
 
 	@Override
 	public boolean resetRBloomFilter() {
 		//当布隆过滤器的容量到达1%的时候就定时清理，进一步降低重复的概率. 算法：布隆过滤器redis实现是容量扩大76倍，此系统目标清理是使用容量达到目标的10%清理。
-		if(nowBloomFilter.count()/nowBloomFilter.getSize()>0.001) { 
-			//先删除历史的布隆过滤器
-			historyBloomFilter.delete();
-			//在将当前布隆过滤器修改为历史布隆过滤器
-			nowBloomFilter.rename(RepositoryPathUtils.buildBloomFilterKey(CommonConstant.History,swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
-			 
-			initBloomFilter();	
-			LOGGER.debug("清理swan存储成功");
+		initBloomFilter();	
+		for(int i=-5;i<=-2;i++) {
+			RBloomFilter<String> bloomFilter = redissonClient.getBloomFilter(RepositoryPathUtils.buildBloomFilterKey(DateUtils.subDay(i),swanConfig.getApplicationName(), swanConfig.getSwanRedisConfig().getRBloomFilterConfig().getName()));
+			bloomFilter.delete();
 		}
 		return true;
 	}
